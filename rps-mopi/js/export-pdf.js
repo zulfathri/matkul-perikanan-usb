c99f9bccc58c85bc3b39554e41c764fe2289b1c8
zulfathri <79937400+zulfathri@users.noreply.github.com>
2025-12-16 09:50:04 +0800

    Update export-pdf.js

--- rps-mopi/js/export-pdf.js
+++ rps-mopi/js/export-pdf.js
@@ -1,28 +1,11 @@
-fetch('data/rps.json')
-  .then(res=>res.json())
-  .then(data=>renderRPS(data));
+function exportPDF(){
+  const { jsPDF } = window.jspdf;
+  const doc = new jsPDF('p','mm','a4');
 
-function renderRPS(rpsData){
-  let html = `<table><thead><tr><th>Minggu</th><th>Sub-CPMK</th><th>Detail</th></tr></thead><tbody>`;
-  rpsData.forEach((m,i)=>{
-    html += `
-      <tr class="week" onclick="toggle(${i})">
-        <td>${m.minggu}</td>
-        <td>${m.subcpmk}</td>
-        <td>Klik untuk detail</td>
-      </tr>
-      <tr id="detail${i}" class="hidden">
-        <td colspan="3">
-          <strong>Materi:</strong> ${m.materi}<br>
-          <strong>Metode:</strong> ${m.metode}<br>
-          <strong>Penilaian:</strong> ${m.penilaian}
-        </td>
-      </tr>`;
+  doc.html(document.getElementById('content'), {
+    callback: () => doc.save('RPS_MOPI_2025.pdf'),
+    x:10,
+    y:10,
+    width:190
   });
-  html += '</tbody></table>';
-  document.getElementById('rpsTable').innerHTML = html;
-}
-
-function toggle(i){
-  document.getElementById('detail'+i).classList.toggle('hidden');
 }
